924346ba3c7827d5539fa7fb32a4783f981d6e29
Bruno Oliveira de Souza <brunosouza22397@gmail.com>
2020-02-21 14:22:22 -0300

    novas classes

--- src/CaixaEletronico.php
+++ src/CaixaEletronico.php
@@ -1,7 +1,7 @@
 <?php
 namespace Reweb\Job\Backend;
 
-use Conta;
+use Reweb\Job\Backend\Conta;
 
 class CaixaEletronico
 {
@@ -17,38 +17,44 @@ class CaixaEletronico
     *
     *   @var Conta
     */
-    // TODO: Criar algum tipo de middleware para este tipo de transação,
-    //       intanciar a classe inteira de outro usuário é inseguro.
     private $contaDestino;
 
-    public function __construct($id) {
-        $this->conta = new Conta($id);
+    public function __construct( int $id, string $pass ) {
+        $this->conta = new Conta($id, $options = array("pass" => $pass));
     }
 
-    public function deposito($val) {
+    /**
+     * Insere $val ao saldo da conta e imprime resposta ao usuário.
+     * 
+     * @return string
+     */
+    public function deposito(float $val) {
         try {
             if ($val > 0) {
                 $c = $this->conta;
-
+                
                 echo "====================================================\n";
-                echo "Conta: $c->getId()\n";
-                echo "Tipo: $c->getTipo()\n";
-                echo "Saldo antes da transação: B$ $c->getSaldo()\n";
+                echo "--------------COMPROVANTE DE DEPÓSITO--------------\n";
+                echo "====================================================\n";
+                echo "Conta: ". $c->getId() ."\n";
+                echo "Tipo: ". $c->getTipo() ."\n";
+                echo "Saldo antes da transação: B$ ". $c->getSaldo() ."\n";
 
                 $c->deposito($val);
 
                 echo "====================================================\n";
                 echo "Valor depositado: B$ $val\n";
-                echo "Saldo após transação: B$ $c->getSaldo()\n";
+                echo "Saldo após transação: B$ ". $c->getSaldo() ."\n";
                 echo "\n";
                 echo "Obrigado por nos confiar com suas finanças! :)\n";
             } else {
+                echo "====================================================\n";
                 echo "Valor inválido para depósito.";
             }
 
         } catch(Exception $e) {
             echo "====================================================\n";
-            echo "Ocorreu um erro ao realizar seu depósito. :( \n"
+            echo "Ocorreu um erro ao realizar seu depósito. :( \n";
             echo "Entre em contato com nosso suporte e passe a seguinte\n";
             echo "informação: \n";
             echo $e;
@@ -56,33 +62,53 @@ class CaixaEletronico
         }
     }
 
-    public function saque($val) {
+
+    /**
+     * Remove $val do saldo da conta e imprime resposta ao usuário.
+     * 
+     * @return string
+     */
+    public function saque(float $val) {
         try {
             $c = $this->conta;
 
-            // TODO: criar mensagens de erro individuais para cada caso.
-            if ( $val > 0 && $val !> $c->getSaldo() && $val !> $c->getLimiteSaque() ) {
+            if ( $val > 0 ) {
+                if ( !($val > $c->getSaldo()) ) {
+                    if ( !($val > $c->getLimiteSaque()) ) {
+                        echo "====================================================\n";
+                        echo "---------------COMPROVANTE DE SAQUE-----------------\n";
+                        echo "====================================================\n";
+                        echo "Conta: ". $c->getId() ."\n";
+                        echo "Tipo: ". $c->getTipo() ."\n";
+                        echo "Saldo antes da transação: B$ ". $c->getSaldo() ."\n";
 
-                echo "====================================================\n";
-                echo "Conta: $c->getId()\n";
-                echo "Tipo: $c->getTipo()\n";
-                echo "Saldo antes da transação: B$ $c->getSaldo()\n";
+                        $c->saque($val);
 
-                $c->saque($val);
-
-                echo "====================================================\n";
-                echo "Valor do saque: B$ $val\n";
-                echo "Taxa de saque: B$ $c->getTaxaSaque()\n";
-                echo "Saldo após transação: B$ $c->getSaldo()\n";
-                echo "\n";
-                echo "Obrigado por nos confiar com suas finanças! :)\n";
+                        echo "====================================================\n";
+                        echo "Valor do saque: B$ $val\n";
+                        echo "Taxa de saque: B$ ". $c->getTaxaSaque() ."\n";
+                        echo "Saldo após transação: B$ ". $c->getSaldo() ."\n";
+                        echo "\n";
+                        echo "Obrigado por nos confiar com suas finanças! :)\n";
+                    } else {
+                        echo "====================================================\n";
+                        echo "Valor maior que seu limite de Saque.\n";
+                        echo "Seu limite de saque é de: ". $c->getLimiteSaque() ."\n";
+                    }
+                } else {
+                    echo "====================================================\n";
+                    echo "Valor maior do que o saldo disponível.\n";
+                    echo "Seu saldo é: ". $c->getSaldo() ."\n";
+                }
+                
 
             } else {
+                echo "====================================================\n";
                 echo "Valor inválido para saque.\n";
             }
         } catch ( Exception $e ) {
             echo "====================================================\n";
-            echo "Ocorreu um erro ao realizar seu saque. :( \n"
+            echo "Ocorreu um erro ao realizar seu saque. :( \n";
             echo "Entre em contato com nosso suporte e passe a seguinte\n";
             echo "informação: \n";
             echo $e;
@@ -90,33 +116,48 @@ class CaixaEletronico
         }
     }
 
-    public function transferencia($id, $val) {
+    /**
+     * Realiza transferência entre as contas e retorna resposta ao usuário.
+     * 
+     * @return string
+     */
+    public function transferencia(int $id, float $val) {
         try {
             $c = $this->conta;
 
             $this->contaDestino = new Conta($id);
             $cd = $this->contaDestino;
 
-            if ( $val > 0 && $val !> $c->getSaldo() ) {
+            if ( $val > 0 ) {
+                if ( !($val > $c->getSaldo()) ) {
+                    
+                    echo "====================================================\n";
+                    echo "-------------COMPROVANTE DE TRANSFERÊNCIA-----------\n";
+                    echo "====================================================\n";
+                    echo "Conta: ". $c->getId() ."\n";
+                    echo "Tipo: ". $c->getTipo() ."\n";
+                    echo "Saldo antes da transação: B$ ". $c->getSaldo() ."\n";
+                    echo "\n";
+                    echo "Conta de Destino: ". $cd->getId() ."\n";
+                    echo "Tipo: ". $cd->getTipo() ."\n";
 
-                echo "====================================================\n";
-                echo "Conta: $c->getId()\n";
-                echo "Tipo: $c->getTipo()\n";
-                echo "Saldo antes da transação: B$ $c->getSaldo()\n";
-                echo "\n";
-                echo "Conta de Destino: $cd->getId()\n";
-                echo "Tipo: $cd->getTipo()\n";
+                    $c->transferencia($val);
+                    $cd->deposito($val);
 
-                $c->transferencia($val);
-                $cd->deposito($val);
+                    echo "====================================================\n";
+                    echo "Valor da transferência: B$ $val\n";
+                    echo "Saldo após transação: B$ ". $c->getSaldo() ."\n";
+                    echo "\n";
+                    echo "Obrigado por nos confiar com suas finanças! :)\n";
 
-                echo "====================================================\n";
-                echo "Valor da transferência: B$ $val\n";
-                echo "Saldo após transação: B$ $c->getSaldo()\n";
-                echo "\n";
-                echo "Obrigado por nos confiar com suas finanças! :)\n";
+                } else {
+                    echo "====================================================\n";
+                    echo "Valor maior que seu saldo disponível.\n";
+                    echo "Seu saldo é de: ". $c->getSaldo();
+                }
 
             } else {
+                echo "====================================================\n";
                 echo "Valor inválido para transferência.\n";
             }
         } catch (Exception $e) {
--- src/Conta.php
+++ src/Conta.php
@@ -1,29 +1,36 @@
 <?php
 namespace Reweb\Job\Backend;
 
-// TODO: criar validação com senha
 class Conta
 {
     private $id;
+    private $autenticado;
     private $tipo;
-    private float $limiteSaque;
-    private float $taxaSaque;
-    private float $saldo;
+    private $limiteSaque;
+    private $taxaSaque;
+    private $saldo;
 
-    public function __construct($id) {
+    public function __construct(int $id, $options = array()) {
         $this->id = $id;
+        
+        if ( isset($options['pass']) ){
+            $this->autenticado = $this->autenticarConta($id, $options['pass']);
+        } else {
+            $this->autenticado = false;
+        }
+
         $conta = $this->buscarConta($id);
         $this->tipo = $conta['tipo'];
-        $this->saldo = $conta['saldo'];
+        $this->saldo = $this->autenticado ? $conta['saldo'] : 0;
         switch ($this->tipo) {
             case 'corrente':
                 $this->limiteSaque = 600;
-                $this->taxaSaque = 2,5;
+                $this->taxaSaque = 2.5;
                 break;
 
             case 'poupança':
                 $this->limiteSaque = 1000;
-                $this->taxaSaque = 0,8;
+                $this->taxaSaque = 0.8;
                 break;
 
             default:
@@ -31,22 +38,6 @@ class Conta
                 $this->taxaSaque = 0;
                 break;
         }
-    }
-
-    /**
-    *   Busca conta no banco de dados e retorna array com $tipo e $saldo
-    *   ou erro caso a conta não seja encontrada.
-    *
-    *   @return array
-    */
-    // TODO: CRIAR MIDDLEWARE PARA BD
-    private function buscarConta($id) {
-        // [...]Funções de busca no banco.
-        if (!$conta[]) {
-            return $erro;
-        }
-
-        return $conta[];
     }
 
     public function getId() {
@@ -70,23 +61,100 @@ class Conta
     }
 
     /**
+    *   Busca conta no banco de dados e retorna array com $tipo e $saldo caso autenticado
+    *   ou somente $tipo caso não autenticado. Retorna erro caso a conta não seja encontrada.
+    *
+    *   @return array
+    */
+    private function buscarConta(int $id) {
+        
+        if ($this->autenticado) {
+            // Aqui ficariam as funções de busca autenticada no banco.
+            switch ( $id ) {
+                case 1:
+                    return $conta = array(
+                        "tipo" => "corrente",
+                        "saldo" => '4800'
+                    );
+                    break;
+                case 2:
+                    return $conta = array(
+                        "tipo" => "poupança",
+                        "saldo" => '10000'
+                    );
+                    break;
+                default:
+                    return $erro = array(
+                        "401 - Usuário não encontrado"
+                    );
+            }
+        } else {
+            // Aqui ficariam funções de busca não autenticada no banco.
+
+            switch ( $id ) {
+                case 1:
+                    return $conta = array(
+                        "tipo" => "corrente"
+                    );
+                    break;
+                case 2:
+                    return $conta = array(
+                        "tipo" => "poupança"
+                    );
+                    break;
+                default:
+                    return $erro = array(
+                        "401 - Usuário não encontrado"
+                    );
+            }
+        }
+        
+    }
+
+    /**
+     * Validação de senha. Retorna true para sucesso e false para falha.
+     * 
+     * @return bool
+     */
+    private function autenticarConta(int $id, string $pass) {
+        //Aqui ficaria a validação da conta no BD
+        switch ($id) {
+            case 1:
+                if ($pass == "pass1") {
+                    return true;
+                }
+                return false;
+                break;
+            case 2:
+                if ($pass == "pass2") {
+                    return true;
+                }
+                return false;
+                break;
+            default:
+                return false;
+                break;
+        }
+    }
+
+    /**
     *   Recebe valor e o adiciona ao saldo da conta.
     */
-    public function deposito($val) {
+    public function deposito(float $val) {
         $this->saldo += $val;
     }
 
     /**
     *   Recebe valor + taxa de saque e subtrai do saldo da Conta
     */
-    public function saque($val) {
+    public function saque(float $val) {
         $this->saldo -= ($val + $this->getTaxaSaque());
     }
 
     /**
-    *   Recebe valor + taxa de saque e subtrai do saldo da Conta
+    *   Recebe valor e subtrai do saldo da Conta
     */
-    public function transferencia($val) {
+    public function transferencia(float $val) {
         $this->saldo -= $val;
     }
 }
